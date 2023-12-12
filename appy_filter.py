727ae28f1d374ed854d7cd2e6d640df85240bf26
Ericnguyen89 <127569176+Ericnguyen89@users.noreply.github.com>
2023-12-12 15:55:35 +0700

    Create appy_filter.py

---
+++ appy_filter.py
@@ -0,0 +1,48 @@
+import sys
+from gvm.connections import UnixSocketConnection
+from gvm.protocols.gmp import Gmp
+from gvm.transforms import EtreeCheckCommandTransform
+from lxml import etree
+#from python-gvm.gvm.errors import GvmError
+from gvm.errors import GvmError
+
+path = '/run/gvmd/gvmd.sock'
+connection = UnixSocketConnection(path=path)
+transform = EtreeCheckCommandTransform()
+output_file = "your_xml_file.xml"
+username = 'admin'
+password = 'CTN@1305'
+
+def run_gvm_command(gmp, report_ids):
+    try:
+        report_data = gmp.get_report(report_id=report_ids, details=True)
+        return report_data
+    except GvmError as e:
+        print(f"Error running GVM command: {e}")
+        return None
+
+try:
+    with Gmp(connection=connection, transform=transform) as gmp:
+        gmp.authenticate(username, password)
+        reports = gmp.get_reports()
+        report_ids = reports.xpath('//report/@id')[-1]
+        print(f"Report Latest ID: {report_ids}")
+
+        report_data = run_gvm_command(gmp, report_ids)
+        if report_data:
+            detail_reports = gmp.get_report(str(report_ids), filter_string="apply_overrides=0 levels=hml rows=1000 min_qod=70 first=1 sort-reverse=severity notes=1 overrides=1", details=True)
+
+            # Convert the XML element to a string
+            xml_string = etree.tostring(detail_reports, pretty_print=True, encoding="utf-8").decode("utf-8")
+
+            # Write the string to the file
+            with open(output_file, "w") as file:
+                file.write(xml_string)
+
+            print(f"Report downloaded and saved to {output_file}")
+        else:
+            print("Failed to download the report.")
+
+except GvmError as e:
+    print('An error occurred', e, file=sys.stderr)
+
